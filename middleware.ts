9303eb65d0cbeb5ee3ef71a4c13c1f81b12b52c8
Priyanshu Sinha <priyanshusinha.2807@gmail.com>
2024-08-19 21:08:21 +0530

    bugs fixing nad file structuring

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher([
     "/sign-in",
@@ -7,10 +7,10 @@ const isPublicRoute = createRouteMatcher([
     "/",
     "/home"
 ])
-
 const isPublicApiRoute = createRouteMatcher([
     "/api/videos"
 ])
+
 
 export default clerkMiddleware((auth, req) => {
     const { userId } = auth();
@@ -18,20 +18,25 @@ export default clerkMiddleware((auth, req) => {
     const isAccessingDashboard = currentUrl.pathname === "/home"
     const isApiRequest = currentUrl.pathname.startsWith("/api")
 
+    // If user is logged in and accessing a public route but not the dashboard
     if (userId && isPublicRoute(req) && !isAccessingDashboard) {
         return NextResponse.redirect(new URL("/home", req.url))
     }
+    //not logged in
     if (!userId) {
-        if (!isPublicApiRoute(req) && !isPublicRoute(req)) {
+        // If user is not logged in and trying to access a protected route
+        if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
             return NextResponse.redirect(new URL("/sign-in", req.url))
         }
 
+        // If the request is for a protected API and the user is not logged in
         if (isApiRequest && !isPublicApiRoute(req)) {
             return NextResponse.redirect(new URL("/sign-in", req.url))
         }
     }
     return NextResponse.next()
-});
+
+})
 
 export const config = {
     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
